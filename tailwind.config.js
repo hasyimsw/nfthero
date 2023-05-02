aefb3a5667f6d250a6ca4c9f323d5efae6bf219f
Hasyim Sri Wahyudi <hasyimsriewahyudi@gmail.com>
2023-05-03 06:52:29 +0700

    update

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    container: {
+      padding: {
+        DEFAULT: "1rem",
+        sm: "2rem",
+        lg: "2rem",
+        xl: "2rem",
+        "2xl": "2rem",
+      },
+    },
+    extend: {},
+  },
+  plugins: [],
+};
